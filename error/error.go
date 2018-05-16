e2c3fcc07763a06ebea7caadcd06aff1621c2736
Ryan Bastic <ryan@bastic.net>
2018-05-16 09:15:21 +0200

    moving error.go to error/

--- error/error.go
+++ error/error.go
@@ -3,28 +3,26 @@
 // This class is derived from golang.org/src/encoding/gob/error.go
 //
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE.go file.
+// license that can be found in the LICENSE file.
 
 package schemaless
 
 import "fmt"
 
-// Errors in decoding and encoding are handled using panic and recover.
-// Panics caused by user error (that is, everything except run-time panics
-// such as "index out of bounds" errors) do not leave the file that caused
-// them, but are instead turned into plain error returns. Encoding and
-// decoding functions and methods that do not return an error either use
-// panic to report an error or are guaranteed error-free.
+const (
+	prefix = "schemaless: "
+)
 
-// A SchemalessError is used to distinguish errors (panics) generated in this package.
-type Schemaless struct {
+// A SchemalessError is used to distinguish errors (panics) generated from Schemaless
+// behavior rather than system behavior itself.
+type SchemalessError struct {
 	err error
 }
 
 // Errorf is like error_ but takes Printf-style arguments to construct an error.
-// It always prefixes the message with "gob: ".
+// It always prefixes the message with "schemaless: ".
 func errorf(format string, args ...interface{}) {
-	error_(fmt.Errorf("gob: "+format, args...))
+	error_(fmt.Errorf(prefix+format, args...))
 }
 
 // Error wraps the argument error and uses it as the argument to panic.
@@ -36,10 +34,10 @@ func error_(err error) {
 // plain error. It overwrites the error return of the function that deferred its call.
 func CatchError(err *error) {
 	if e := recover(); e != nil {
-		ge, ok := e.(SchemalessError)
+		se, ok := e.(SchemalessError)
 		if !ok {
 			panic(e)
 		}
-		*err = ge.err
+		*err = se.err
 	}
 }
